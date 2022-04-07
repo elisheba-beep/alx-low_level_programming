d030854612fcc823985628caacd133f6803ecc8e
Oluwatomiwa <ekunife35@gmail.com>
2022-04-07 09:47:32 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*to compile without warning*/
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "_Wunused-parameter"
+/**
+ * main - this is the main function
+ * Return: returns 1
+ */
+int main (void)
+{
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stderr);
+	return(1);
+}
