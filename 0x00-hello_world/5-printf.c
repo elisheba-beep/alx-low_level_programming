97ec85ee3d97d0d196f7a3b4b8bfdfdf0d21fc7e
Oluwatomiwa <ekunife35@gmail.com>
2022-04-07 09:05:10 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,11 +3,12 @@
 /*to compile without warning*/
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "_Wunused-parameter"
-/*main - main function
+/**
+ * main - main function
  * Return: returns 0
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
