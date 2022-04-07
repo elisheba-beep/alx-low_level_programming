37021490a4efe4cea2dca6207cabf92192d556a8
Oluwatomiwa <ekunife35@gmail.com>
2022-04-07 08:20:24 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 
-int main()
+int main(void)
 {
 	/*initializing string*/
 
 	char sentence[] = "\"Programming is like building a multilingual puzzle\n";
-	
 	puts(sentence);
-	
 	return (0);
-}	
+}
