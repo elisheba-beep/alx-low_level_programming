65a22c5e8dcd302ade54001dcbde7181f0cb11d3
Oluwatomiwa <ekunife35@gmail.com>
2022-04-07 09:53:21 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,8 +4,8 @@
  * main - this is the main function
  * Return: returns 1
  */
-int main (void)
+int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
-	return(1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
