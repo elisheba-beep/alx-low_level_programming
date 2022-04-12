77194d6859df659acc23f2cc3fdec5905d119771
Oluwatomiwa <ekunife35@gmail.com>
2022-04-12 07:07:46 -0700

    every minute

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,7 +11,7 @@ void jack_bauer(void)
 
 	for (num1 = 0; num1 <= 23; num1++)
 	{
-		for (num2 = num1 + 1; num2 <= 59; num2++)
+		for (num2 = 0; num2 <= 59; num2++)
 		{
 			_putchar((num1 / 10) + '0');
 			_putchar((num1 % 10) + '0');
@@ -23,4 +23,4 @@ void jack_bauer(void)
 			_putchar('\n');
 		}
 	}
-}	
+}
