7fcdfaf01c7cdd7b139ee9cb720a437e92e4a10b
Oluwatomiwa <ekunife35@gmail.com>
2022-04-12 06:08:41 -0700

    bas

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _abs - gets the absolute value
+ * @int: takes in an int
+ *
+ * Return: returns an int
+ */
+int _abs(int)
+{
+	abs(int);
+	
+	return (0);
+}
