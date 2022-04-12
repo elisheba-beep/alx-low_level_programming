04295658d7f987e643d8c49e96ff12d4aeb82578
Oluwatomiwa <ekunife35@gmail.com>
2022-04-12 15:33:47 -0700

    times tables

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_times_table - prnts times table
+ * @n: input
+ *
+ * Return: void
+ */
+void print_times_table(int n)
+{
+	int i = o;
+
+	while (i < 10)
+	{
+		int j = 0;
+
+		while (j <= n)
+		{
+			if (j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (j == 0)
+				_putchar('0');
+			else if ((i * j) > 9)
+			{
+				_putchar(((i * j) / 10) + '0');
+				_putchar(((i * j) % 10) + '0');
+			}
+			else if (((i * j) < 10) && (j != 0))
+			{
+				_putchar(' ');
+				_putchar(((i * j) % 10) + '0');
+			}
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
