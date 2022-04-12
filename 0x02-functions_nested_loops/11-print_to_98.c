de7bce89fc2e7b3448ec617be62042ea93e8a430
Oluwatomiwa <ekunife35@gmail.com>
2022-04-12 15:02:01 -0700

    n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,69 +9,38 @@
  */
 void print_to_98(int n)
 {
-	for(n = n; n <= 98; n++)
+	if (n < 98)
 	{
-		if (n < 98 && n > 9)
+		while (n <= 98)
 		{
 			_putchar((n / 10) + '0');
 			_putchar((n % 10) + '0');
-			_putchar(',');
-			_putchar(' ');
-		}
-		else if (n < 9 && n > 0)
-		{
-			_putchar((n % 10) + '0');
-			_putchar(',');
-			_putchar(' ');
-		}
-		else if (n < 0 && n > -9)
-		{
-			n = (-1) * n;
-			_putchar('-');
-			_putchar((n % 10) + '0');
-			_putchar(',');
-			_putchar(' ');
-		}
-		else if (n < -9 && n > -99)
-		{
-			n = (-1) * n;
-			_putchar('-');
-			_putchar((n / 10) + '0');
-			_putchar((n % 10) + '0');
-			_putchar(',');
-			_putchar(' ');
-		}
-		else
-		{
-			n = (-1) * n;
-			_putchar('-');
-			_putchar((n / 100) + '0');
-			_putchar((n / 10) + '0');
-			_putchar((n % 10) + '0');
+			if (n != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			n++;
 		}
 	}
-	for(n = n; n >= 98; n--)
+	else if (n > 98)
 	{
-		if (n == 99)
+		while (n >= 98)
 		{
 			_putchar((n / 10) + '0');
 			_putchar((n % 10) + '0');
-			_putchar(',');
-			_putchar(' ');
-		}
-		else
-		{
-			_putchar((n / 100) + '0');
-			_putchar((n / 10) + '0');
-			_putchar((n % 10) + '0');
-			_putchar(',');
-			_putchar(' ');
+			if (n != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			n--;
 		}
 	}
-	if ( n == 98)
+	else
 	{
-		_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
+		_putchar('9');
+		_putchar('8');
 	}
-}	
-		
+	_putchar('\n');
+}
