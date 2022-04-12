bcbc49611876ed8fe04177f4f16e9029f0946cbc
Oluwatomiwa <ekunife35@gmail.com>
2022-04-12 05:12:10 -0700

    a-z x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints a-z n 10 new lines
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char n = 0;
+
+	while (n < 10)
+	{
+		char ch;
+
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+	}
+}
