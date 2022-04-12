149786a68e49b9ac471aad47e0566151c94525b6
Oluwatomiwa <ekunife35@gmail.com>
2022-04-12 05:58:41 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,19 +12,19 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		write (1, "+", 2);
+		write (1, "+", 1);
 		
 		return (1);
 	}
 	else if (n < 0)
 	{
-		write (1, "-", 2);
+		write (1, "-", 1);
 
 		return (-1);
 	}
 	else
 	{
-		write (1, "0", 2);
+		write (1, "0", 1);
 		
 		return (0);
 	}
