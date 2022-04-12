41ca41b73483898829a797397124b49ba1129724
Oluwatomiwa <ekunife35@gmail.com>
2022-04-12 07:05:12 -0700

    24 hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int num1, num2;
+
+	for (num1 = 0; num1 <= 23; num1++)
+	{
+		for (num2 = num1 + 1; num2 <= 59; num2++)
+		{
+			_putchar((num1 / 10) + '0');
+			_putchar((num1 % 10) + '0');
+			_putchar(':');
+			_putchar((num2 / 10) + '0');
+			_putchar((num2 % 10) + '0');
+			if (num1 == 23 && num2 == 59)
+				continue;
+			_putchar('\n');
+		}
+	}
+}	
