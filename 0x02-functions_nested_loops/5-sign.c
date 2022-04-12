0ef2a53267196a2bcd43dd6ba916afcec1ed9e7d
Oluwatomiwa <ekunife35@gmail.com>
2022-04-12 05:52:17 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_sign - prints +, - or 0
+ * @n: takes an int n
+ *
+ * Return: returns 1 if +ve, -1 if -ve and 0 if 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		write (1, '+', 1);
+		
+		return (1);
+	}
+	else if (n < 0)
+	{
+		write (1, '-', 1);
+
+		return (-1);
+	}
+	else
+		write (1, '0', 1);
+		
+		return (0);
+}
