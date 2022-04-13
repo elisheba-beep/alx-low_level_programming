a839ba34c4bbcea3f118b446d483ef6497085dfc
Oluwatomiwa <ekunife35@gmail.com>
2022-04-13 01:39:24 -0700

    times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,34 +9,37 @@
  */
 void print_times_table(int n)
 {
-	int i = 0;
+	int num, mult, prod;
 
-	while (i < 10)
+	if (n >= 0 && n <= 15)
 	{
-		int j = 0;
-
-		while (j <= n)
+		for (num = 0; num <= n; num++)
 		{
-			if (j != 0)
+			_putchar('0');
+		
+			for (mult = 1; mult <= n; mult++)
 			{
 				_putchar(',');
 				_putchar(' ');
+
+				prod = num * mult;
+
+				if (prod <= 99)
+					_putchar(' ');
+				if (prod <= 9)
+					_putchar(' ');
+				if (prod >= 100)
+				{
+					_putchar((prod / 100) + '0');
+					_putchar(((prod / 10)) % 10 + '0');
+				}
+				else if (prod <= 99 && prod >= 10)
+				{
+					_putchar((prod / 10) + '0');
+				}
+				_putchar((prod % 10) + '0');
 			}
-			if (j == 0)
-				_putchar('0');
-			else if ((i * j) > 9)
-			{
-				_putchar(((i * j) / 10) + '0');
-				_putchar(((i * j) % 10) + '0');
-			}
-			else if (((i * j) < 10) && (j != 0))
-			{
-				_putchar(' ');
-				_putchar(((i * j) % 10) + '0');
-			}
-			j++;
+			_putchar('\n');
 		}
-		_putchar('\n');
-		i++;
 	}
 }
