a6dde3d2eeed2f6d6334de416584fd03a0986674
Oluwatomiwa <ekunife35@gmail.com>
2022-04-13 02:01:28 -0700

    fibonacci less than 4 million

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - fibonacci sequence to 4,000,000
+ *
+ * Return: returns 0
+ */
+int main(void)
+{
+	unsigned long fib1 = 0, fib2 = 1, sum;
+	float total;
+
+	while(1)
+	{
+		sum = fib1 + fib2;
+		if (sum > 4000000)
+			break;
+
+		if ((sum % 2) == 0)
+			total += sum;
+
+		fib1 = fib2;
+		fib2 = sum;
+	}
+	printf("%.0f\n", total);
+
+	return (0);
+}
