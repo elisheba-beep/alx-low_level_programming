78537a765b7e9bcc55bd67613bccefa840d7d344
Oluwatomiwa <ekunife35@gmail.com>
2022-04-14 07:29:52 -0700

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -18,12 +18,12 @@ int main(void)
 			printf("Fizz ");
 		else if (i % 5 == 0)
 			printf("Buzz ");
-		else if ((i % 3 == 0) && (i % 5 == 0))
+		else if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz ");
 		else
 			printf("%d ", i);
 	}
-	printf("\n");
+	printf("Buzz\n");
 
 	return (0);
 }
