18735fc1641736b378c3d45f0df9ceee45d44dce
Oluwatomiwa <ekunife35@gmail.com>
2022-04-14 05:41:27 -0700

    square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -11,11 +11,13 @@ void print_square(int size)
 {
 	int i;
 
-	for (i = 0; i <= size; i++)
+	if (size <= 0)
+		_putchar('\n');
+	for (i = 0; i < size; i++)
 	{
 		int j;
 
-		for (j = 0; j <= size; j++)
+		for (j = 0; j < size; j++)
 		{
 			_putchar('#');
 		}
