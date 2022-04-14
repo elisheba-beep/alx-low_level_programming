c070906564b7de308398ec621c9bd7cf458f7bbb
Oluwatomiwa <ekunife35@gmail.com>
2022-04-14 03:19:59 -0700

    print linr

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_line - draws a straight line
+ * @n: number of times _ should be printed
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+	int num;
+	
+	for (num = 0; num <= n; num++)
+	{
+		if (num != 0)
+			_putchar('_');
+	}
+	_putchar('\n');
+}
+	
