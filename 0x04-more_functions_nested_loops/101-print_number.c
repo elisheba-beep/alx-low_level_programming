7927146538bc96f39a391a5a9f2e0cb96c920abf
Oluwatomiwa <ekunife35@gmail.com>
2022-04-14 06:29:01 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_number - prints any number ont character at a time
+ * @n: the number to print
+ *
+ * Return: void
+ */
+void print_number(int n)
+{
+	long n1;
+	long n2 = n;
+
+	if (n2 < 0)
+	{
+		_putchar('-');
+		n2 *= -1;
+	}
+	if (n2 / 100000 != 0)
+	{
+		n1 = n2 / 100000;
+		_putchar(n1 / 10000 % 10 + '0');
+		_putchar(n1 / 1000 % 10 + '0');
+		_putchar(n1 / 100 % 10 + '0');
+		_putchar(n1 / 10 % 10 + '0');
+		_putchar(n1 % 10 + '0');
+	}
+	n1 = n2 % 100000;
+	if (n1 / 10000 % 10 != 0)
+	{
+		_putchar(n1 / 10000 % 10 + '0');
+		_putchar(n1 / 1000 % 10 + '0');
+		_putchar(n1 / 100 % 10 + '0');
+		_putchar(n1 / 10 % 10 + '0');
+	}
+	else if (n1 / 100 % 10 != 0)
+	{
+		_putchar(n1 / 100 % 10 + '0');
+		_putchar(n1 / 10 % 10 + '0');
+	}
+	else if (n1 / 10 % 10 != 0)
+		_putchar(n1 / 10 % 10 + '0');
+	_putchar(n1 % 10 + '0');
+}
