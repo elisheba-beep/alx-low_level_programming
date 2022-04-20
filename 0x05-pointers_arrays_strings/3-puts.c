685d59d820f86484ba3382077e60ef8c7b5d3dc2
Oluwatomiwa <ekunife35@gmail.com>
2022-04-20 03:06:42 -0700

    prints the string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _puts - prints a string followed by a new line
+ * @str: the string to be printed
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+	puts(str);
+}	
