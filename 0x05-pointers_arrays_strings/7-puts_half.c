b6334f32e3fe607eb1141471dd647fa0f6dc7d74
Oluwatomiwa <ekunife35@gmail.com>
2022-04-20 05:34:33 -0700

    half string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * puts_half - prints the second half of a string
+ * @str: the string to put in half
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	char half;
+	int l, mid, i, k;
+
+	l = strlen(str);
+	mid = l / 2;
+	for (i = mid, k = 0; i <= l; i++, k++)
+		half[k] = str[i];
+
+	printf("%s\n", half);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -21,3 +21,5 @@ void puts2(char *str);
 char *_strcpy(char *dest, char *src);
 
 void print_array(int *a, int n);
+
+void puts_half(char *str);
