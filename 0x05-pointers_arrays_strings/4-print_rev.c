7a5eee809be9d09475363da6d7bad64b6bc3c2c7
Oluwatomiwa <ekunife35@gmail.com>
2022-04-20 03:41:52 -0700

    reverse string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,20 +11,9 @@
 void print_rev(char *s)
 {
 	int l, i;
-	char *begin_ptr, *end_ptr, ch;
 
 	l = strlen(s);
-	begin_ptr = s;
-	end_ptr = s;
 
-	for (i = 0; i < l -1; i++)
-		end_ptr++;
-	for (i = 0; i < l / 2; i++)
-	{
-		ch = *end_ptr;
-		*end_ptr = *begin_ptr;
-		*begin_ptr = ch;
-		begin_ptr++;
-		end_ptr--;
-	}
+	for (i = l-1; i >= 0; i--)
+		printf("%c", s[i]);
 }
