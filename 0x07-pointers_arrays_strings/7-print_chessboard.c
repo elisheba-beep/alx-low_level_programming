24de39d1eda51b8839691313bc21cde42a233b8e
elisheba-beep <ekunife35gmail.com>
2022-04-27 00:51:52 +0100

    chess

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,13 +8,15 @@
  */
 void print_chessboard(char (*a)[8])
 {
-	int i;
+	int i, j;
 
-	i = 0;
-	while ((a)[i] != '\0')
+	for (i = 0; i < 8; i++)
 	{
-		_putchar((a)[i]);
-		i++;
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j];
+		}
+
+		_putchar('\n');
 	}
 }
-
