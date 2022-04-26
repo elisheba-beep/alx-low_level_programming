3c323ea9a74083455b69541b4e71136bed637eaf
elisheba-beep <ekunife35gmail.com>
2022-04-26 22:36:42 +0100

    locate char

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ * *_strchr - locates a character
+ * @s: the string
+ * @c: the character to return
+ *
+ * Return: returns a character
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; i < (strlen(s) - 1); i++)
+	{
+		if (s[i] == c)
+			return (*s[i]);
+	}
+	return NULL;
+}
