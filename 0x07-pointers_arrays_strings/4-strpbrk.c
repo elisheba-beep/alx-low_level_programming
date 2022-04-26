10379ca546fca464d70dbd626011b47bf6c473e6
elisheba-beep <ekunife35gmail.com>
2022-04-27 00:11:37 +0100

    match characters

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strpbrk - searches for a string
+ * @s: character
+ * @accept: character
+ *
+ * Return: returns character
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if accept[j] == s[i]
+				return (&s[i]);
+		}
+	}
+	return NULL;
+}
