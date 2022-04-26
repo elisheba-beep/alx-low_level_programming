4ec2c7fa19afc7221dff7929c61cb8b4d551eaba
elisheba-beep <ekunife35gmail.com>
2022-04-26 22:51:37 +0100

    character

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -4,7 +4,7 @@
 
 
 /**
- * *_strchr - locates a character
+ * _strchr - locates a character
  * @s: the string
  * @c: the character to return
  *
@@ -14,11 +14,17 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; i < (strlen(s) - 1); i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == c)
-			
+		{
 			return (s);
+		}
+		else
+		{
+			s[i] = s[i + 1];
+		}
+
 	}
 	return NULL;
 }
