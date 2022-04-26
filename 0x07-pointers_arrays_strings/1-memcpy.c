de4a802f81d4089f982d9e1656813a338ca4e5e4
elisheba-beep <ekunife35gmail.com>
2022-04-26 22:04:47 +0100

    copies memory

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_memcpy - copies memory area
+ * @*dest: character
+ * @*src: character
+ * @n: number of bytes
+ *
+ * Return: returns a character
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	n--;
+	while (n > 0)
+	{
+		dest[n] = src[n]
+		n--;
+	}
+}
+
