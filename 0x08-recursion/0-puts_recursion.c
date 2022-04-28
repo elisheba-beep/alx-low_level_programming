479d011851a106ec0b043cdb47657ec974df0437
elisheba-beep <ekunife35gmail.com>
2022-04-28 10:38:07 +0100

    puts recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string and a new line
+ * @s: the string
+ *
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	if (s = '\0')
+		_putchar('\n')
+	_putchar(s + _puts_recursion(s - 1);
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,6 @@
+/**
+ * my header file recursion
+ */
+int _putchar(char c);
+
+void _puts_recursion(char *s);
