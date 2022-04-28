04e770fcee726385db3344c75cb9c94ae5bb0966
elisheba-beep <ekunife35gmail.com>
2022-04-28 12:01:11 +0100

    puts recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,12 +9,12 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s != '\0')
 	{
-		_putchar('\n');
-		continue;
+		_putchar(*s);
+		_puts_recursion (s + 1);
 	}
 
-	_putchar(*s);
-	_puts_recursion (s + 1);
+	*s = '\0';
+	_putchar('\n');
 }
