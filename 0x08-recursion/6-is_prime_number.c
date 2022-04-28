3afae24fbd90b77db0e7c2d12f47229b57d585d4
elisheba-beep <ekunife35gmail.com>
2022-04-28 17:14:39 +0100

    prime numbers

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -11,15 +11,16 @@ int is_prime_number(int n)
 {
 	int i;
 
-	i = 2;
-
-	if (n <= 2)
-		return (n == 2) ? 1 : 0;
+	i = n/2;
+	if (i == 1)
+		return (1);
 	if (n % i == 0)
-		return 0;
+		return (0);
 	if (i * i > n)
-		return 1;
-	i++;
-
-	return is_prime_number(n);
+		return (0);
+	else
+	{
+		i = i - 1;
+		is_prime_number(n);
+	}
 }
