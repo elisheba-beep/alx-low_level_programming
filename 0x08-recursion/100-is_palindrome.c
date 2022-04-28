103e5e66531310e14535850f13b972ac3f000843
elisheba-beep <ekunife35gmail.com>
2022-04-28 18:10:41 +0100

    palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * is_palindrome - checks for an empty string
+ * @s: string
+ *
+ * Return: returns 1 if true amd 0 if false
+ */
+int is_palindrome(char *s)
+{
+	if (*s == '\0')
+		return (1);
+
+	return (0);
+}
