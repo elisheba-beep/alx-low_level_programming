f67461a6d4fd498f8f7e619bc320364c3f27c5bd
elisheba-beep <ekunife35@gmail.com>
2022-05-02 14:29:10 +0100

    name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - prints the name of this program
+ * @argc: the size of array argv[]
+ * @argv[]: the array with size argc
+ *
+ * Return: returns 0
+ */
+int main(int argc, char* argv[])
+{
+	int i = 0;
+	_putchar(argv[i]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,4 @@
+/**
+ * my header file
+ */
+int _putchar(char c);
