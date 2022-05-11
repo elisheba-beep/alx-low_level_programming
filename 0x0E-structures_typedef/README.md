27cc45597ba9552f48b523004464a61277c2329f
elisheba-beep <ekunife35@gmail.com>
2022-05-11 09:21:31 +0100

    new dog struct

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+learning about strutures in c
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * struct dog - a new dog structure
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ *
+ * Description: gives information about any dog structure
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
