2b9562bf1a7a98c8d8c0dd402960da24fa0d24f6
elisheba-beep <ekunife35@gmail.com>
2022-05-25 00:53:22 +0100

    print list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * list_len - prints number of elements in a linked list
+ * @h: pointer to the first element in the list
+ *
+ * Return: returns the number of elements
+ */
+size_t list_len(const list_t *h)
+{
+	unsigned int count;
+	const list_t *list;
+
+	list = h;
+
+	for (count = 0; list; count++)
+		list = list->next;
+
+	char[] result = " elements";
+
+	return (count + result);
