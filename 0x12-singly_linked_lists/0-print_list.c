f5693d0162435cf154d227d78c5779d504cf40cb
elisheba-beep <ekunife35@gmail.com>
2022-05-25 00:29:05 +0100

    print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - prints the elements in the list_t list
+ * @h: pointer to the first element in the list
+ *
+ * Return: returns the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	const list_t *first;
+	unsigned int count;
+
+	first = h;
+
+	for (count = 0; first; count++)
+	{
+		if (str == NULL)
+		{
+			printf("[0] (nil)");
+		}
+		printf("[%u] %s", first->len, first->str);
+		first->next = first;
+	}
+}
