80ec27a95ff7c9de38494b7f93b9f39d837d4d5e
elisheba-beep <ekunife35@gmail.com>
2022-05-25 00:39:56 +0100

    print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -16,7 +16,9 @@ size_t print_list(const list_t *h)
 
 	for (count = 0; first; count++)
 	{
+		if (str == NULL)
+			printf("[0] (nil)");
 		printf("[%u] %s", first->len, first->str);
-		first->next = first;
+		first = first->next;
 	}
 }
