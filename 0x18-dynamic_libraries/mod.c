b795b6acb4268af898a01027af4fafd893c826cb
elisheba-beep <ekunife35@gmail.com>
2022-07-12 14:22:56 -0700

    operations

---
+++ 0x18-dynamic_libraries/mod.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+
+/**
+ * mod - the modulus
+ * @a: number one
+ * @b: number two
+ *
+ * Return: returns the modulus
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
---
+++ 0x18-dynamic_libraries/mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two numbers
+ * @a: number one
+ * @b: number two
+ *
+ * Return: returns the multiplied value
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
